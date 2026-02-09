7f9262e128dacd476f845e6d2e3234a6959dc1c3
Ashish Pal <ak47pal29@gmail.com>
2026-02-09 22:03:55 +0530

    Fix frontend dist path for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 import path from "path";
 
@@ -18,21 +18,20 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production"
-      ? true
-      : "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production" ? true : "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
