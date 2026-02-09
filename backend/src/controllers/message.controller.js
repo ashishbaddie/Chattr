d266265cef3ef847962b53e04533a53f6ef412fb
Ashish Pal <ak47pal29@gmail.com>
2026-02-09 19:06:41 +0530

    Project Completed

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,81 @@
+import prisma from "../lib/prisma.js";
+import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user.id;
+    const filteredUsers = await prisma.user.findMany({
+      where: {
+        id: { not: loggedInUserId },
+      },
+      select: {
+        id: true,
+        fullName: true,
+        email: true,
+        profilePic: true,
+      },
+    });
+    res.status(200).json(filteredUsers);
+  } catch {
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user.id;
+
+    const messages = await prisma.message.findMany({
+      where: {
+        OR: [
+          { senderId: myId, receiverId: parseInt(userToChatId) },
+          { senderId: parseInt(userToChatId), receiverId: myId },
+        ],
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in getMessages controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        senderId,
+        receiverId: parseInt(receiverId),
+        text,
+        image: imageUrl,
+      },
+    });
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
